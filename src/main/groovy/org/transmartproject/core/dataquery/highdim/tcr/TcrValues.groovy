add94f5d157789a2f7a1bc52bc774437827dffce
cognizant <cognizant@amre1al304.pcld.pfizer.com>
2017-01-11 08:10:38 -0500

    Added new tcr folder and groovy file for TCR HighdiM

---
+++ src/main/groovy/org/transmartproject/core/dataquery/highdim/tcr/TcrValues.groovy
@@ -0,0 +1,8 @@
+package org.transmartproject.core.dataquery.highdim.tcr
+
+public interface TcrValues {
+	String getNtSequence();
+	String getAaSequence();
+	Integer getCount();
+	Double getFreqCount();
+}
